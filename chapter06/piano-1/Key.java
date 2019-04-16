e5de9fd0f9aa0b07e27514fce3eec64d56ac871a
Liam Davidson <ldavidson@lcs.on.ca>
2019-04-16 14:03:11 -0400

    done

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -2,27 +2,73 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
-    
+
     GreenfootImage keyDown;
     GreenfootImage keyUp;
-    
-    public Key()
+    GreenfootImage blackKeyDown;
+    GreenfootImage blackKeyUp;
+    boolean isKeyWhite;
+    String keyToPress;
+    String noteToPlay;
+    boolean canPlaySound;
+
+    public Key(String key, String note, boolean isWhite)
     {
         keyDown = new GreenfootImage("white-key-down.png");
         keyUp = new GreenfootImage("white-key.png");
-        setImage(keyUp);
+        blackKeyDown = new GreenfootImage("black-key-down.png");
+        blackKeyUp = new GreenfootImage("black-key.png");
+        isKeyWhite = isWhite;
+        keyToPress = key;
+        noteToPlay = note;
+        canPlaySound = true;
+
+        if (isKeyWhite == true) {
+            setImage(keyUp);
+        } else if (isKeyWhite == false) {
+            setImage(blackKeyUp);
+        }
     }
 
-    /**
-     * Do the action for this key.
-     */
     public void act()
     {
-        if (Greenfoot.isKeyDown("g")) {
-            setImage(keyDown);
+        animate();
+    }
+
+    void animate() {
+        if (Greenfoot.isKeyDown(keyToPress) && canPlaySound == true) {
+            if (getImage() == keyUp) {
+                setImage(keyDown);
+            }
+            if (getImage() == blackKeyUp) {
+                setImage(blackKeyDown);
+            }
+            Greenfoot.playSound(noteToPlay + ".wav");
+            canPlaySound = false;
+
+        } else if (Greenfoot.isKeyDown(keyToPress) && canPlaySound == false) {
+            if (getImage() == keyUp) {
+                setImage(keyDown);
+            }
+            if (getImage() == blackKeyUp) {
+                setImage(blackKeyDown);
+            }
+
+            
         } else {
-            setImage(keyUp);
+            if (getImage() == blackKeyDown) {
+                setImage(blackKeyUp);
+            }
+            if (getImage() == keyDown) {
+                setImage(keyUp);
+            }
+
+            canPlaySound = true;
         }
+    }
+
+    void playNote() {
+        Greenfoot.playSound(noteToPlay + ".wav");   
     }
 }
 
--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -9,6 +9,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Piano extends World
 {
     int timeElapsed;
+    int addKeyVar;
+    int addBlackKeyVar;
+    int whichKey;
+    int whichNote;
+    int whichBlackKey;
+    int whichBlackNote;
+    String[] keyToPress = {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"};
+    String[] noteToPlay = {"3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c", "4d", "4e", "4f"}; 
+    String[] blackKeyToPress = {"w", "e", "r", "y", "u", "o", "p", "["};
+    String[] blackNoteToPlay = {"3c#", "3d#", "3f#", "3g#", "3a#", "4c#", "4d#", "4f#"}; 
+
     String[] peopleInClass = {"Julio", "Liam", "Simon", "Eric"}; 
 
     /**
@@ -17,18 +28,49 @@ public class Piano extends World
     public Piano() 
     {
         super(800, 340, 1);
-        
+        addKeyVar = 75;
+        addBlackKeyVar = 75;
         timeElapsed = 0;
+        whichKey = 0;
+        whichNote = 0;
+        whichBlackKey = 0;
+        whichBlackNote = 0;
+        
     }
 
     public void act() {
-        
-        showText("Time Elapsed: " + timeElapsed, 120, 20);
+
+        showText("Time Elapsed: " + timeElapsed, 120, 320);
         if (timeElapsed % 60 == 0 && timeElapsed / 60 < 4) {
             // showText("hello " + peopleInClass[timeElapsed / 60], 400, 170);
             // showText("Array index is " + timeElapsed / 60, 400, 250);
         }
 
+        if (addKeyVar < 740 && timeElapsed % 10 == 0) {
+            createKeys();
+            addKeyVar += 64;
+        }
+
+        if (addBlackKeyVar < 700 && timeElapsed > 5 && timeElapsed % 10 == 0) {
+            if (addBlackKeyVar != 267 && addBlackKeyVar != 459) {
+                createBlackKeys();
+            }
+            addBlackKeyVar += 64;
+        }
+
         timeElapsed += 1;
+
+    }
+
+    void createKeys() {
+        addObject(new Key(keyToPress[whichKey], noteToPlay[whichNote], true), addKeyVar, 140);
+        whichKey += 1;
+        whichNote += 1;
+    }
+
+    void createBlackKeys() {
+        addObject(new Key(blackKeyToPress[whichBlackKey], blackNoteToPlay[whichBlackNote], false), addBlackKeyVar + 30, 80);
+        whichBlackKey += 1;
+        whichBlackNote += 1;
     }
 }
